fd2cb359892f048ca18bd2e88dd5d0ddd845b728
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-28 15:10:33 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251028/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(6);
+    cout << 13 << " * " << 0.165 << " = " << (13 * 0.165);
+    return 0;
+}
