4bd8208f667d67ddc7efda263ca81398ea54f340
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-28 15:14:32 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251028/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(2);
+
+    double f;
+    cin >> f;
+    cout << f;
+    return 0;
+}
