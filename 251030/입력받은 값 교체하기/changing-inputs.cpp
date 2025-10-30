7fc5e3e2d4a430a272ec346e63717ba4a56f63b9
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-30 21:54:18 +0900

    Title: 입력받은 값 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251030/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, tmp;
+    cin >> a >> b;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+
+    cout << a << ' ' << b;
+    return 0;
+}
