0fd8891b978ec5356623aad9b8453acedfba6f5b
jang-taehyun <71426382+jang-taehyun@users.noreply.github.com>
2025-10-31 14:05:59 +0900

    Title: 알파벳과 사칙연산, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251031/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+string expression;
+int num['f' - 'a'];
+int res = 0;
+
+int Cal()
+{
+    stack<char> op;
+    int op1 = num[expression[0] - 'a'];
+    int op2;
+    for(int i=1; i<expression.length(); ++i)
+    {
+        if('a' <= expression[i] && expression[i] <= 'f')
+        {
+            op2 = num[expression[i] - 'a'];
+            if('+' == op.top())
+                op1 += op2;
+            else if('-' == op.top())
+                op1 -= op2;
+            else
+                op1 *= op2;
+        }
+        else
+            op.push(expression[i]);
+    }
+
+    return op1;
+}
+
+void Backtracking(int cnt)
+{
+    if(cnt >= 'f' - 'a')
+    {
+        int tmp = Cal();
+        res = (tmp > res ? tmp : res);
+        return;
+    }
+
+    for(int i=1; i<=4; ++i)
+    {
+        num[cnt] = i;
+        Backtracking(cnt+1);
+    }
+}
+
+int main() {
+    cin >> expression;
+
+    // Please write your code here.
+    Backtracking(0);
+    cout << res;
+    return 0;
+}
